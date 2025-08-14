2d5ff4a42ae5dda46a355ff6f31b761b069882bf
Felipe Costa Muniz <133247483+felip-000@users.noreply.github.com>
2025-08-14 10:57:00 -0300

    learn(ap01-javascript): encerra secao 2.1.4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,73 @@
-// //node -- watch nomedoarquivo.js, toda mudança no código faz com que o node atualize o programa, então ele vai estar sempre compilando sem precisar ficar escrevendo no terminal
+//node --watch nomedoarquivo.js, toda mudança no código faz com que o node atualize o programa, então ele vai estar sempre compilando sem precisar ficar escrevendo no terminal
+
+
+
+// let calculadora = {
+//     //pode ser arrow function
+//     somar: (a, b) => a + b,
+//     //e função comum também
+//     subtrair: function (a, b) {
+//         return a - b;
+//     },
+//     soma: 2,
+// };
+// console.log(`2 + 3 = ${calculadora.somar(2, 3)}`);
+// console.log(`2 - 3 = ${calculadora.subtrair(2, 3)}`);
+// console.log(calculadora.soma);    
+
+// let concessionaria = {
+// cnpj: "00011122210001-45",
+// endereco: {
+//     logradouro: "Rua A",
+//     numero: 10,
+//     bairro: "Vila J",
+// },
+// veiculos: [
+//     {
+//         marca: "Ford",
+//         modelo: "Ecosport",
+//         anoDeFabricacao: 2018,
+//     },
+//     {
+//         marca: "Chevrolet",
+//         modelo: "Onix",
+//         anoDeFabricacao: 2020,
+//     },
+//     {
+//         marca: "Volkswagen",
+//         modelo: "Nivus",
+//         anoDeFabricacao: 2020,
+//     },
+// ],
+// };
+// for (let veiculo of concessionaria.veiculos) {
+//     console.log(`Marca: ${veiculo.marca}`);
+//     console.log(`Modelo: ${veiculo.modelo}`);
+//     console.log(`Ano de Fabricação: ${veiculo.anoDeFabricacao}`);
+//     //Posso ter elementos adicionais que não vai dar erro, só aparecer undefined, podendo colocar no código depois, mas é uma prática ruim
+//     console.log(`Capacidade da Bateria: ${veiculo.kw}`);
+//     console.log(`Capacidade do Tanque: ${veiculo.litros}`);
+//     console.log(`Autonomia: ${veiculo.autonomia}`);
+// }   
+
+// let pessoa = {
+//     nome: "João",
+//     idade: 17,
+// }
+// //o acesso a propriedades pode ser feito com ponto
+// console.log("Me chamo " + pessoa.nome);
+// //e com [] também
+// console.log("Tenho " + pessoa["idade"] + " anos");
+
+// let pessoaComEndereco = {
+//     nome : "Pedro",
+//     idade: 21,  
+//     endereco:{
+//         logradouro: "Rua B",
+//         numero: 121,
+//     }
+// };
+// console.log(`Sou ${pessoaComEndereco.nome}, tenho ${pessoaComEndereco.idade} anos e moro na ${pessoaComEndereco.endereco.logradouro}, número ${pessoaComEndereco.endereco.numero}`);
 
 // function eAgora(){
 //     let cont = 1
