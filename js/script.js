1eb06b8b632a52d581a1f35dde6fc50dee0c8a07
felip-000 <felipecostamuniz@gmail.com>
2025-08-21 10:56:38 -0300

    learn(ap01-javascript): Apostila Finalizada

--- js/script.js
+++ js/script.js
@@ -1,5 +1,142 @@
 //node --watch nomedoarquivo.js, toda mudança no código faz com que o node atualize o programa, então ele vai estar sempre compilando sem precisar ficar escrevendo no terminal
 
+const axios = require("axios");
+//sua chave aqui
+const appid = "23c97a3560cd85ae7dd240c47bce2fdb";
+//cidade desejada
+const q = "Itu";
+//unidade de medida de temperatura
+const units = "metric";
+//idioma
+const lang = "pt_BR";
+//quantidade de resultados
+const cnt = "10"
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${q}&units=${units}&appid=${appid}&lang=${lang}&cnt=${cnt}`;
+
+//faz a requisição
+axios
+.get(url)
+.then((res) => {
+ //mostra o resultado e devolve somente a parte de interesse
+ console.log(res);
+ return res.data;
+})
+.then((res) => {
+ //mostra o total e devolve o resultado
+ console.log(res.cnt);
+ return res;
+})
+.then((res) => {
+    //devolve somente a lista de previsões
+    console.log("aqui", res);
+    return res["list"];
+})
+.then((res) => {
+    //para cada resultado, mostra algumas informações
+    for (let previsao of res) {
+        console.log(`
+        ${new Date(+previsao.dt * 1000).toLocaleString()},
+        ${'Min: ' + previsao.main.temp_min}\u00B0C,
+        ${'Max: ' + previsao.main.temp_max}\u00B0C,
+        ${'Hum: ' + previsao.main.humidity}%,
+        ${previsao.weather[0].description}
+        `);
+    }
+    return res;
+})
+.then((res) => {
+    //verifica quantas previsões têm percepção humana de tempertura acima de 30 graus
+    const lista = res.filter(r => r.main.feels_like >= 30);
+    console.log (`${lista.length} previsões têm percepção humana de temperatura acima de 30 graus`)
+});
+
+// function fatorial(n) {
+//     if (n < 0) return Promise.reject("Valor não pode ser negativo");
+//     let res = 1;
+//     for (let i = 2; i <= n; i++) res *= i;
+//     return Promise.resolve(res);
+// }    
+
+// function chamadaComThenCatch() {
+//     fatorial(5)
+//     .then((res) => console.log(res))
+//     .catch((res) => console.log(res));
+//     fatorial(-1)
+//     .then((res) => console.log(res))
+//     .catch((res) => console.log(res));
+// }
+// chamadaComThenCatch();  
+
+// //para usar await tem que ser async
+// async function chamadaComAwait() {
+//     //note que não há paralelismo implícito
+//     //somente haverá paralelismo se a função chamada utilizar explicitamente
+//     try{
+//         const f1 = await fatorial(5);
+//         console.log(f1);
+    
+//         const f2 = await fatorial(-1);
+//         console.log(f2);       
+//     } 
+//     catch (erro){
+//         console.log(erro);
+//     }
+// }
+// chamadaComAwait();
+
+// async function hello(nome) {
+//     return "Oi, " + nome;
+// }
+// const boasVindas = hello("João");
+// console.log(boasVindas);
+// boasVindas.then((res) => console.log(res));
+
+// function calculoRapidinho(numero) {
+//     return numero >= 0
+//     ? Promise.resolve((numero * (numero + 1)) / 2)
+//     : Promise.reject("Somente valores positivos, por favor");
+// }
+// //55
+// calculoRapidinho(10)
+// .then((resultado) => {
+//     console.log(resultado);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+// //Somente valores positivos, por favor
+// calculoRapidinho(-1)
+// .then((resultado) => {
+//     console.log(resultado);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+// //Imprime o "esperando..." antes de tudo
+// console.log("esperando...");
+
+// function calculoRapidinho (numero){
+//     return Promise.resolve((numero * (numero + 1)) / 2);
+// }
+// calculoRapidinho (10).then(resultado =>{
+//     console.log (resultado)
+// })
+// //Executa primeiro, mesmo que a promise já esteja fullfilled
+// console.log('Esperando...')    
+
+// function calculoDemorado(numero) {
+//     return new Promise(function (resolve, reject) {
+//         let res = 0;
+//         for (let i = 1; i <= numero; i++) {
+//             res += i;
+//         }
+//         resolve(res);
+//     });
+// }
+// calculoDemorado(10).then( (resultado) => {
+//     console.log(resultado);
+// })
+    
 // const fs = require('fs')
 // const abrirArquivo = function(nomeArquivo){
 //     const exibirConteudo = function(erro, conteudo){
